0721ac17cd49182f3dd15984ea7def71ec03ad1e
puncy22@gmail.com <puncy22@gmail.com>
2016-06-02 00:55:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Spremenljivke
+stevilo = 159
+
+drugo_stevilo = 34
+
+tretje_stevilo = stevilo + drugo_stevilo
+
+ime = "moje ime"
+
+res = True
+
+narobe = False
+
+ime = "Filip"
+
+novo_ime = raw_input("Vpisi svoje ime")
+
+print("Pozdravljeni %s, lepo, da si tu" %novo_ime)
+print("Pozdravljen: " + novo_ime + ", lepo da si tu!")
